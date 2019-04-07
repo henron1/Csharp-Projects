39294840d5063951a9f73a53d18aa16717931d8d
Lambda_School_Loaner_98 <lambda_school_loaner_98@Lambda-School-Loaner-98s-MacBook-Pro.local>
2019-04-07 16:11:41 -0400

    c# calculator

---
+++ myApp/Calculator.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace myApp
+{
+    class Calculator
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter another number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(num1+ num2);
+        }
+    }
+}
--- myApp/Program.cs
+++ myApp/Program.cs
@@ -1,12 +1,16 @@
-﻿using System;
+﻿// using System;
 
-namespace myApp
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("The current time is " + DateTime.Now);
-        }
-    }
-}
+// namespace myApp
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             Console.WriteLine("The current time is " + DateTime.Now);
+//             Console.WriteLine("     /|");
+//             Console.WriteLine("    / |");
+//             Console.WriteLine("   /  |");
+//             Console.WriteLine("  /   |");
+//         }
+//     }
+// }
